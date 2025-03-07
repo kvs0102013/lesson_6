5e9b65fd3f04ca2ac9f51bb475bd8fdd7c87d0b9
kvs0102013 <kvs0102013@gmail.com>
2025-03-07 15:33:31 +0300

    task6.2

--- task6.1.py
+++ task6.1.py
@@ -1,6 +1,12 @@
-number = int(input('Введите число: '))
+# number = int(input('Введите число: '))
+# total = 0
+# while number != 0:
+#     total += number
+#     number = int(input('Введите число: '))
+# print(total)
+
+step = int(input('Введите шаг прогрессии  '))
 total = 0
-while number != 0:
-    total += number
-    number = int(input('Введите число: '))
-print(total)
+while total < 93:
+    total +=step
+    print(total)
