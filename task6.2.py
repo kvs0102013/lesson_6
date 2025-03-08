daa45432bfc4adff30de8ca8028a298783b25d76
kvs0102013 <kvs0102013@gmail.com>
2025-03-08 18:08:01 +0300

    task6.3.2

--- task6.2.py
+++ task6.2.py
@@ -11,15 +11,28 @@
 #     total +=step
 #     print(total)
 
-water = int(input('Температура: '))
-meter = 0
-while water > 15:
-    meter += 20
-    water -= 2
-    if water <= 15:
+# water = int(input('Температура: '))
+# meter = 0
+# while water > 15:
+#     meter += 20
+#     water -= 2
+#     if water <= 15:
+#         break
+#     meter +=10
+# print('Прошел дистанцию ', meter)
+
+number = int(input('Введите число  '))
+summ = 0 # Считаем сумму всех чисел до появления  5
+while number != 0:
+    last_num = number % 10
+    print(last_num)
+    if last_num == 5:
+        print('Обнаружен разрыв ')
         break
-    meter +=10
-print('Прошел дистанцию ', meter)
+    summ += last_num
+    number = number // 10
+print('Сумма чисел равна  ', summ)    
+
     
     
     
