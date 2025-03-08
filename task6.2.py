8954c05f0b070e34446b4251f636c44dd93fd964
kvs0102013 <kvs0102013@gmail.com>
2025-03-08 21:35:09 +0300

    task6.3.4

--- task6.2.py
+++ task6.2.py
@@ -33,19 +33,31 @@
 #     number = number // 10
 # print('Сумма чисел равна  ', summ)    
 
-book_lib =int(input('Сколько книг выдал библиотекарь? '))
-book_watch = int(input('Сколько книг просмотрено? '))
-book_res = int(input('Сколько требует реставрации? '))
-summ_res = 0
-summ_res += book_res
-while summ_res < 5:
-    book_watch = int(input('Сколько книг просмотрено? '))
-    book_res = int(input('Сколько требует реставрации? '))
-    summ_res += book_res
-    print('Требует реставрации ', summ_res)
-    print('На сегодня все благодарю за помощь')
-print('Ура практика завершена')
+# book_lib =int(input('Сколько книг выдал библиотекарь? '))
+# book_watch = int(input('Сколько книг просмотрено? '))
+# book_res = int(input('Сколько требует реставрации? '))
+# summ_res = 0
+# summ_res += book_res
+# while summ_res < 5:
+#     book_watch = int(input('Сколько книг просмотрено? '))
+#     book_res = int(input('Сколько требует реставрации? '))
+#     summ_res += book_res
+#     print('Требует реставрации ', summ_res)
+#     print('На сегодня все благодарю за помощь')
+# print('Ура практика завершена')
+    
+st_summ = int(input('Введите стартовую сумму '))
+num = int(input('Какая цифра выпала? '))
+while num != 3:
+    print('Вы выиграли 500 рублей ')
+    num = int(input('Какая цифра выпала? '))
+else:        
+    print('Вы проиграли все')
+    print('Игра закончена')
+    print('На счету осталось 0 рублей')
     
     
+    # comment: 
+# end while    
     
     
