299814bfce9136002cca8accc63129558faa795f
kvs0102013 <kvs0102013@gmail.com>
2025-03-08 17:45:56 +0300

    task6.3.1

--- task6.2.py
+++ task6.2.py
@@ -5,8 +5,21 @@
 #     number = int(input('Введите число: '))
 # print(total)
 
-step = int(input('Введите шаг прогрессии  '))
-total = 0
-while total < 93:
-    total +=step
-    print(total)
+# step = int(input('Введите шаг прогрессии  '))
+# total = 0
+# while total < 93:
+#     total +=step
+#     print(total)
+
+water = int(input('Температура: '))
+meter = 0
+while water > 15:
+    meter += 20
+    water -= 2
+    if water <= 15:
+        break
+    meter +=10
+print('Прошел дистанцию ', meter)
+    
+    
+    
