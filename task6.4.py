9005030eb475649c2279dd0ba60fd5ad53de92bd
kvs0102013 <kvs0102013@gmail.com>
2025-03-09 10:26:49 +0300

    task6.4.2

--- task6.4.py
+++ task6.4.py
@@ -1,8 +1,17 @@
-count = 10 #первоначальный таймер
-while count <= 10:
-    if count == 0:
-        print('Время вышло!')
+# count = 10 #первоначальный таймер
+# while count <= 10:
+#     if count == 0:
+#         print('Время вышло!')
+#         break
+#     else:
+#         print(count)
+#         count -= 1
+
+active = int(input('Продолжаем работать? 1/0'))
+while True:
+    active = int(input('Продолжаем работать? 1/0'))
+    if active == 0:
+        print('Приложение закрываеться')
+        print('Работа завершена')
         break
-    else:
-        print(count)
-        count -= 1
+    
